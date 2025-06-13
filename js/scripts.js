67499127b18cd8a66ffee3571ca2bef41bac8b2f
Filipp0v <fn.filippov7@gmail.com>
2025-06-13 19:00:26 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,9 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.querySelector('.nav-toggle');
-  const menu = document.querySelector('.nav-menu');
-  toggle.addEventListener('click', () => {
-    menu.classList.toggle('open');
+document.addEventListener("DOMContentLoaded", function() {
+  const toggle = document.querySelector(".nav-toggle");
+  const menu = document.querySelector(".nav-menu");
+
+  toggle.addEventListener("click", function() {
+    menu.classList.toggle("open");
   });
 });
